10e3f60110fb228656ac10f10022073cb34375b5
Ryan Rampersad <ryan@Ryans-MacBook-Pro-2.local>
2018-10-27 23:52:37 -0500

    first hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, useState } from 'react';
+
+function FieldComponent({ label, value }) {
+  const [name, setName] = useState('');
+
+  return (
+    <div>
+      <h1 className="title">{label}</h1>
+      <input
+        style={{
+          border: 0,
+          borderBottom: '1px solid #000',
+        }}
+        className="subtitle"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
-    return <div className="App">Hooks</div>;
+    return (
+      <div className="App">
+        <section className="section">
+          <div className="container">
+            <FieldComponent label="First Name" value="Hi" />
+          </div>
+        </section>
+      </div>
+    );
   }
 }
 
